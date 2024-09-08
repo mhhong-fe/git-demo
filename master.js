756733e8597fd10985da2384c63507f0f9d4ba4e
mhhong <hongminghui@baijia.com>
2024-09-08 10:34:10 +0800

    feat: 修改master555

--- master.js
+++ master.js
@@ -2,10 +2,10 @@ console.log("master 111");
 
 console.log("master 222");
 
+console.log("master 999");
+
 console.log("master 333");
+
 console.log("master 666");
 
 console.log("master 777");
-console.log("master 888");
-
-console.log("master 999");
